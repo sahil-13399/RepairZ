bde276191a12fc714ae6df199bbcebac0da6c507
Sahil Samantaray <sahil@Sahils-MacBook-Air.local>
2020-12-25 19:00:33 +0530

    Initial Commit

---
+++ src/main/java/com/repairz/repzimpl/RepzimplApplication.java
@@ -0,0 +1,14 @@
+package com.repairz.repzimpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepzimplApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepzimplApplication.class, args);
+        System.out.println("Hello World");
+    }
+
+}
---
+++ src/test/java/com/repairz/repzimpl/RepzimplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repairz.repzimpl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepzimplApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
